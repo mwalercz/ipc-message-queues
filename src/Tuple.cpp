0e0c965fd1de3d9ec5520be2a3cd61f3a38e9aa6
Maciej Kulawik <kulawik.repo@gmail.com>
2016-05-13 14:07:20 +0200

    Remove invalid comment in Tuple.cpp (int:* case)

--- src/Tuple.cpp
+++ src/Tuple.cpp
@@ -2,7 +2,6 @@
 
 bool Tuple::isMatch(const Query& query) const {
     Query::QueryParts parts = query.getParts();
-    //tutaj jeszcze nalezy dodac sytuacje jesli w query jest part typu obojętny, np int:*
     for (auto p : parts) {
         Length idx = p.getIdx();
         if (! p.isMatch(elements_.at(idx)) ) {
