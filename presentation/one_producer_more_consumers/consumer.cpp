11fb2ac7b81be389184e5435bbcc3af2bf436110
mwalercz <mwalerczuk@gmail.com>
2016-06-06 03:22:12 +0200

    added more into presentation skeleton

---
+++ presentation/one_producer_more_consumers/consumer.cpp
@@ -0,0 +1,8 @@
+//
+// Created by maciek on 06/06/16.
+//
+#include "LindaClient.hpp"
+int main() {
+    // dummy main
+    return 0;
+}
---
+++ presentation/one_producer_more_consumers/producer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by maciek on 06/06/16.
+//
+
+#include "LindaClient.hpp"
+
+int main() {
+
+}
