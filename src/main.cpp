64f0da36fea4e05e5338805ef16d9c30aa7a529d
Maciej Kulawik <kulawik.repo@gmail.com>
2016-06-09 04:19:00 +0200

    Add main cpp forkmaster :P

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,53 @@
-#include "Tuple.hpp"
+#include <iostream>
+#include <vector>
+#include "unistd.h"
+
 #include "Server.hpp"
+#include "LindaClient.hpp"
+
+static const int n = 100;
+
+
+void autoconsumer() {
+    sleep(5);
+    LindaClient client("/tmp/queues_keys");
+    timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    while(1) {
+        client.output(std::to_string(getpid()));
+        client.read("integer:="+std::to_string(getpid()), tv);
+        client.input("integer:="+std::to_string(getpid()), tv);
+    }
+}
 
 int main() {
-    // dummy main
+    std::vector<int> pids;
+    std::cout << "Preparing processes:\n\t";
+    for(int i=0; i < n; ++i) {
+        int pid;
+        if ((pid=fork()) == 0) {
+            autoconsumer();
+            return 0;
+        }
+        pids.push_back(pid);
+        std::cout << i << " started.\r\t";
+    }
+    std::cout << "\nStarting server... ";
+    try {
+        Server server("/tmp/queues_keys");
+        std::cout << "done. Starting processes. Serving." << std::endl;
+        server.serve();
+    } catch(std::exception& e) {
+        std::cout << "abort: " << e.what() << std::endl;
+        std::cout << "kill -9";
+        for(int i : pids) {
+            std::cout << " " << i;
+        }
+        std::cout << std::endl;
+        return 1;
+    }
+
+
     return 0;
 }
