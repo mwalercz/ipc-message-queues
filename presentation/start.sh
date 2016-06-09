f811c95d24a2ecd13fc42aa21ca65425b216297d
Łukasz Neumann <linfuinea@gmail.com>
2016-06-09 09:48:31 +0200

    removed server from presentation

--- presentation/start.sh
+++ presentation/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-./build/one_producer_one_consumer/t1_server &
-sleep 1
 for i in {1..10}
 do
    ./build/one_producer_one_consumer/t1_producer &
-   ./build/one_producer_one_consumer/t1_consumer &
+   ./build/one_producer_one_consumer/t1_consumer
 done
