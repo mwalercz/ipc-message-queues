cce9bb193f9eb1cdb2313b76877c00dc11d27d85
Łukasz Neumann <linfuinea@gmail.com>
2016-06-09 09:53:27 +0200

    filepath as argument to server binary

--- presentation/start.sh
+++ presentation/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..10}
+for i in {1..50}
 do
    ./build/one_producer_one_consumer/t1_producer &
    ./build/one_producer_one_consumer/t1_consumer
--- src/server_service.cpp
+++ src/server_service.cpp
@@ -3,10 +3,15 @@
 
 #include "Server.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2){
+        std::cout << "Usage: ./server filepath" << std::endl;
+        exit(-1);
+    }
+    std::string filepath = argv[1];
     std::cout << "Starting server..." << std::endl;
     try {
-        Server server("/tmp/queues_keys");
+        Server server(filepath);
         server.serve();
     } catch(std::exception& e) {
         std::cout << "abort: " << e.what() << std::endl;
